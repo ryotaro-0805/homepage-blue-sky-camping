84bb368ef375c795c2eb2ad94d84df4970b37c6d
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-05-14 14:15:53 +0900

    ✅ 進めました

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ const getMenu = document.getElementById('hamberger');
 const getUl = document.getElementById('companyNameUl');
 const handleClose = () => {
     getUl.classList.remove('active');
+    getIcon.textContent = 'menu';
 };
-
+const getIcon = document.getElementById('menu');
 
 const handleClick = () => {
     getUl.classList.toggle('active');
     if (getUl.classList.value === 'active') {
         window.addEventListener('scroll', handleClose)
+        getIcon.textContent = 'close';
+    } else {
+        getIcon.textContent = 'menu';
     };
 }
 getMenu.addEventListener('click', handleClick);
