4a3a29b5dbfad8baba29525a32c012a5c501514a
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-05-14 16:01:28 +0900

    ✅ 進めました

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const getTitle = document.getElementsByClassName('companyName');
-
-const handleTitle = () => {
-    getTitle[0].classList.toggle('change');
-}
-
-getTitle[0].addEventListener('click', handleTitle);
-
 const getMenu = document.getElementById('hamberger');
 const getUl = document.getElementById('companyNameUl');
 const handleClose = () => {
