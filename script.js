bd4d2cee15d9024ef4627b42eb9099640b0add67
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-05-20 23:49:19 +0900

    ✅ 進めました

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ const handleClick = () => {
 }
 getMenu.addEventListener('click', handleClick);
 
+// swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: 'fade',
+    direction: 'horizontal',
+    loop: true,
+    autoplay: { delay: 3500 },
+    speed: 1500,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
+  // swiper ここまで
